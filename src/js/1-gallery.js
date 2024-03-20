4d8be4ab69a3cfd1bb977dd197045f051fa50cf6
MarinaV27 <Vasilishinamari@gmail.com>
2024-03-20 23:54:34 +0200

    task 1

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,46 +1,36 @@
 
+  import SimpleLightbox from "simplelightbox";
+
+  import "simplelightbox/dist/simple-lightbox.min.css";
+
+  import {images} from "./gallery-images.js";
+
     const container = document.querySelector("ul.gallery");
-    const img = document.querySelectorAll(".galleri-link")
+
     container.innerHTML = creatMarkup(images);
-    container.addEventListener("click", handleModalOpen);
+
    
     function creatMarkup (arr){
       return arr.map(({preview, original, description}) => 
-      `<li class="gallery-item" >
-       <a class="gallery-link" href="${original}">
-         <img
-           class="gallery-image"
-           src="${preview}"
-           data-source="${original}"
-           alt="${description}"
-           width = '360' 
-           height = '200' 
-         />
-       </a>   
-      </li>`
+      `<li class="gallery-item">
+      <a class="gallery-link" href="${original}">
+          <img 
+              class="gallery-image" 
+              src="${preview}" 
+              alt="${description}" 
+              />
+      </a>
+  </li>`
       )
       .join("");
   }
-  // Описаний в документації
-import SimpleLightbox from "simplelightbox";
-// Додатковий імпорт стилів
-import "simplelightbox/dist/simple-lightbox.min.css";
 
 
-    function handleModalOpen(event){
-        if(event.currentTarget === event.target) return;
-        event.preventDefault();
-    const currentImage = event.target.dataset.source;
-    const image = images.find(({original}) => original === currentImage);
- 
-    const instance = basicLightbox.create( 
-       
-          `<img
-            class="gallery-image"
-            src="${image.original}"
-            alt="${image.description}" 
-          />`
-    ) ;
-    instance.show();
+  container.addEventListener("click", handleModalOpen);
 
-    }
+  function handleModalOpen(event) {
+    event.preventDefault();
+    const modal = new SimpleLightbox('.gallery  a', {  captionsData : "alt", captionDelay : 250  });
+  }
+
+
